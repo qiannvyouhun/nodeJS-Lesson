80564a90739b081e082244f35d0dce7a7911762e
qiannvyouhun <2504189164@qq.com>
2019-10-22 08:19:51 +0800

    .

--- (2) NodeJS全局API/test02-1/02decodeBuffer.js
+++ (2) NodeJS全局API/test02-1/02decodeBuffer.js
@@ -1,3 +1,3 @@
 var base64Str = 'emhhbmdzYW46MTIzNDU2';
-var buf=Buffer.from(base64Str,"base64");
-console.log(buf.toString("utf8"));
+var buf2 = Buffer.from(base64Str,"base64");//转换为base64
+console.log(buf2.toString("utf8"));//转换为utf-8并且读取
